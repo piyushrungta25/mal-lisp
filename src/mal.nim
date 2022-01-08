080468f9aa75757fb41abe4d734c5e925da9a5ae
Piyush Rungta <piyushrungta25@gmail.com>
2022-01-08 19:36:04 +0530

    Implement eval and load-fine

--- src/mal.nim
+++ src/mal.nim
@@ -25,17 +25,29 @@ proc rep(str: string, prelude: var ReplEnv): string =
 proc registerSelfHostedFunctions(prelude: var ReplEnv) =
   let functions = @[
     "(def! not (fn* (a) (if a false true)))",
-    "(def! factorial (fn* (a) (if (= a 1) 1 (* (factorial (- a 1)) a) )))"
+    "(def! factorial (fn* (a) (if (= a 1) 1 (* (factorial (- a 1)) a) )))",
+    """(def! load-file (fn* (f) (eval (read-string (str "(do " (slurp f) "\nnil)")))))"""
   ]
-
 
   for fun in functions:
     discard fun.rep(prelude)
 
 
+# required to satisfy the GC
+proc envClosure(env: ReplEnv): MalEnvFunctions =
+  var envirn = env
+  return proc(ast: varargs[MalData]): MalData = eval(ast[0], envirn)
+
+
+proc registerEval(prelude: var ReplEnv) =
+  prelude.set(newSymbol("eval"), MalData(dataType: Function, fun: envClosure(prelude)))
+
+
 when isMainModule:
   var prelude = getPrelude()
   prelude.registerSelfHostedFunctions
+  prelude.registerEval
+
 
   while true:
     let inputLine = getInputLine()
