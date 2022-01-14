bf457a403131ab9cbf97c2154674f5d629ec8701
Piyush Rungta <piyushrungta25@gmail.com>
2022-01-14 15:43:20 +0530

    Add helper function in core

--- src/mal.nim
+++ src/mal.nim
@@ -26,7 +26,8 @@ proc rep(str: string, prelude: var ReplEnv): string =
 proc registerSelfHostedFunctions(prelude: var ReplEnv) =
   let functions = @[
     "(def! not (fn* (a) (if a false true)))",
-    """(def! load-file (fn* (f) (eval (read-string (str "(do " (slurp f) "\nnil)")))))"""
+    """(def! load-file (fn* (f) (eval (read-string (str "(do " (slurp f) "\nnil)")))))""",
+    """(defmacro! cond (fn* (& xs) (if (> (count xs) 0) (list 'if (first xs) (if (> (count xs) 1) (nth xs 1) (throw "odd number of forms to cond")) (cons 'cond (rest (rest xs)))))))""",
   ]
 
   for fun in functions:
